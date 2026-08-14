0634cb9229fe713982ed7c4710fc0ad8a81a6fdc
Bilal Ay <aybilal2340@gmail.com>
2026-08-14 23:37:44 +0300

    pc-inventory projesinin tamami eklendi

---
+++ src/main/java/com/bilal/pc_inventory/PcInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.bilal.pc_inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcInventoryApplication.class, args);
+	}
+
+}
